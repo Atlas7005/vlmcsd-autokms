90eb6217898871f0e2c8d0cad528dcbe895f0521
Atlas <68791999+Atlas7005@users.noreply.github.com>
2021-04-21 03:35:10 +0200

    :)

--- install.bat
+++ install.bat
@@ -1,21 +1,41 @@
-@echo off
-cd %~dp0
-echo Installing TAP driver...
-bin\tap-windows-installer.exe
-echo Installing now the KMS server service...
-bin\vlmcsd.exe -s -U /n -O .
-echo Adding firewall exception...
-netsh advfirewall firewall add rule name="vlmcsd" dir=in action=allow program=%~dp0bin\vlmcsd.exe enable=yes
-echo Starting vlmcsd service...
-net start vlmcsd
-start notepad keys.txt
-set /p kmsKey=Enter a valid key for your Windows version: 
-echo Installing key...
+@ECHO OFF
+ 
+CD %~dp0
+ 
+ECHO [vlmscd: Windows Activator]
+ECHO:
+ 
+:: Check admin privileges
+NET SESSION >nul 2>&1
+IF NOT %ERRORLEVEL% EQU 0 (
+    ECHO You must run the installer as an admin!
+    PAUSE > nul
+    EXIT
+)
+ 
+IF NOT EXIST "%ProgramFiles%\TAP-Windows" (
+	ECHO Installing TAP Driver...
+	bin\tap-windows-installer.exe
+)
+ 
+ECHO Installing the KMS (Key Management Service) service
+bin\vlmcsd.exe -s -U /n -O .=192.168.123.1/24
+ 
+ECHO Adding firewall exception
+NETSH advfirewall firewall add rule name="vlmcsd" dir=in action=allow program=%~dp0bin\vlmcsd.exe enable=yes
+ 
+ECHO Starting vlmcsd service
+NET START vlmcsd
+ 
+START notepad Keys.txt
+SET /p kmsKey=Enter a valid key for your Windows edition:
+ECHO Installing key...
 slmgr /ipk %kmsKey%
-echo Setting up KMS address...
-slmgr /skms 10.10.10.10
-echo Sending activation request...
+ECHO Setting up KMS address...
+slmgr /skms 192.168.123.2
+ECHO Sending activation request...
 slmgr /ato
-echo All done! Press enter to exit.
-pause > nul
-exit
+ECHO All done! Press enter to exit.
+PAUSE > nul
+ 
+EXIT
